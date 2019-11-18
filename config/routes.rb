24b08500c32cac341c5d546a8da8902e38070305
Will Hodgson <will.hodgson@current-rms.com>
2019-11-18 13:33:56 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Add your routes here
-  match '/auth/:provider/callback', to: 'session#create', via: [:get, :posts]
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  
   get '/home', to: 'welcome#home'
   root to: 'welcome#home'
 end
